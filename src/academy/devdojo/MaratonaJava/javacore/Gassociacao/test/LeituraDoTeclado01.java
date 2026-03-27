213fd4b8f6dd9a62316d827268592819f407f60b
Murilo <licleto@hotmail.com>
2026-03-26 21:23:09 -0300

    Adicionando Classes

---
+++ src/academy/devdojo/MaratonaJava/javacore/Gassociacao/test/LeituraDoTeclado01.java
@@ -0,0 +1,16 @@
+package academy.devdojo.MaratonaJava.javacore.Gassociacao.test;
+
+import java.util.Scanner;
+
+public class LeituraDoTeclado01 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite seu nome:");
+        String nome = entrada.nextLine();
+        System.out.println("Digite sua idade:");
+        int idade = entrada.nextInt();
+        System.out.println("Digite seu sexo:");
+        char sexo = entrada.next().charAt(0);
+        System.out.println("Seu nome é: " + nome + ", sua idade é: " + idade + ", seu sexo é: " + sexo);
+    }
+}
